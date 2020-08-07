8065c4959e454481140d82fa937c7d285ed76d60
Mahno-or <62501315+Mahno-or@users.noreply.github.com>
2020-08-07 12:36:18 +0500

    Add files via upload

---
+++ CreateDatabase.php
@@ -0,0 +1,18 @@
+<?php
+// Create connection
+$conn = new mysqli("localhost", "root", "");
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Create database
+$sql = "CREATE DATABASE BookMyShow";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
+?>
