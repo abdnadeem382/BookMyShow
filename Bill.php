42f83d56beb7215ede819d7e777afb4db5a4be2e
Abdullah Nadeem <abdnadeem382@gmail.com>
2020-08-10 17:01:46 +0500

    Bill.php added

---
+++ Bill.php
@@ -0,0 +1,65 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Bill.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div id="main">
+    	<a href="Home.html"><img class="logo" src = "Logo.png" alt="main"></a>
+    	<div class="titlebar">
+    		<a class="nav" href="Home.html"><i class="fa fa-home"></i><textsize> Home</textsize></a>
+        	<a class="nav" href="AboutUs.html"><i class="fa fa-star"></i><textsize> About Us</textsize></a>
+        	<a class="nav" href="ContactUs.html"><i class="fa fa-phone"></i><textsize> Contact Us</textsize></a>
+        </div>
+    </div>
+    <div class ="main">
+    
+        <div class= "heading">
+            <h1>THANK YOU FOR VISITING!</h1>
+        </div>
+        <div class ="bill">
+            <div id= "billHeading">
+                <h1>Bill Details</h1>
+            </div>
+            <div id= "nameAndPhone">
+                <div class="name">
+                    <p>abdullah nadeem</p></br>
+                </div>
+                <div class="phone">
+                    <p>03345009516</p>
+                </div>
+            </div>
+            <div id="movieAndTime">
+                <div class="movie">
+                    <p>Avengers</p>
+                </div>
+                <div class="time">
+                    <p>10pm-12:30pm</p>
+                </div>
+            </div>
+            <div>
+            <div id="seats">
+                <p>Seats: </p>
+                <p>A1,A2,A3</p>
+            </div>
+            <div id="price">
+                <p>Total Price: </p>
+                <p>1000 PKR</p>
+            </div>
+            </div>
+        </div>
+        <div id="SeeYou">
+            <h1>SEE YOU SOON!</h1>
+        </div>
+        </div>
+    <div class ="footer">
+    <div><p class = "contact"><text><i class="fa fa-phone" aria-hidden="true"></i>   +92 311 1222742</text><br><br/></p></div>
+    <div><p class ="contact"><text><i class="fa fa-envelope" aria-hidden="true"></i>  ask@bookmyshow.pk</text><br><br/></p></div>
+    <div><p class ="contact"><text><i class="fa fa-globe" aria-hidden="true"></i>  www.bookmyshow.pk</text></p></div>
+
+  </div>        
+</body>
+</html>
